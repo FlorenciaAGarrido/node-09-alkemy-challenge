5b82331fdb4861511e8ad884b8b703ce76a7b226
florencia garrido <flhor.garrido@gmail.com>
2021-09-18 01:42:31 -0300

    Configuracion de conexion a base de datos

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,19 +1,22 @@
-const ExpressServer = require('./server/expressServer');
-const mongooseLoader = require('./mongoose');
-const config = require('../config');
-const logger = require('./logger');
+const ExpressServer = require("./server/expressServer");
+const sequelize = require("./sequelize");
+const config = require("../config");
+const logger = require("./logger");
 
 module.exports = async () => {
-
-    await mongooseLoader();
-    logger.info('DB loaded and connected');
+  try {
+    await sequelize.authenticate();
+    logger.info("DB loaded and connected");
 
     const server = new ExpressServer();
-    logger.info('Express Loaded');
+    logger.info("Express Loaded");
 
     server.start();
-    logger.info(`###############################
-      Server listening on port: ${config.port}   
-      ###############################`)
-
-}
+    logger.info(`###################################
+      Server listening on port:  ${config.port}
+      ############################
+      `);
+  } catch (error) {
+    console.log("Unable to connect to the database", error);
+  }
+};
